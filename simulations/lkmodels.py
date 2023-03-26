44e76b1b8156184ac1cf2e14ce07c9411239e719
sebastianguinazu <sebastianguinazu@gmail.com>
2023-03-26 17:18:23 -0300

    primeros desarrollos y pruebas de smartbagged

--- simulations/lkmodels.py
+++ simulations/lkmodels.py
@@ -106,3 +106,59 @@ def scores_bagged_clf(X_seed, X_poblacion, random_state, T=50, clf='rf', **kwarg
         n[idx_oob] += 1
     scores = pred / n
     return scores
+
+def scores_smartbagged_clf(X_seed, X_poblacion, random_state, T=50, clf='rf', l1=1, l2=1, **kwargs_clf):
+    """
+    Returns avg of oob predictions of classifier para la poblacion
+    Param:
+        - T number of baggint iteractions 
+        - clf: base estimator (one of rg, logistic)
+    """
+    # K: size of boostrap sample (= size of seed)
+    K = X_seed.shape[0]
+    # U: size of poblation
+    U = X_poblacion.shape[0]
+    # se entrena con una muestra balanceada
+    # vector target: primero seed - luego poblacion
+    y_train = np.concatenate([np.ones(K), np.zeros(K)])
+    # initialize numerador de predicciones
+    pred = np.zeros(U)
+    # initialize denominador de predicciones
+    n = np.zeros(U)
+    # initialize feature importance
+    importance = np.zeros(X_seed.shape[1])
+    # iniialize weight vectors
+    w_poblacion = np.ones(U)
+    w_seed = np.ones(K)
+
+    # bagging
+    for t in range(T):
+        # get sample
+        idx_train = np.random.choice(U, K, replace=True)
+        X_train = np.concatenate([X_seed, X_poblacion[idx_train,:]])
+        # weights
+        print(w_poblacion[idx_train], "/n")
+        weights = np.concatenate([w_seed, w_poblacion[idx_train]])      
+        # train
+        if clf=='rf':
+            clf = RandomForestClassifier(**kwargs_clf)
+        if clf=='logistic':
+            clf = LogisticRegression(**kwargs_clf)
+        if clf=='tree':
+            clf = DecisionTreeClassifier(**kwargs_clf)
+        if clf=='knn':
+            clf = KNeighborsClassifier(**kwargs_clf)
+        clf.fit(X_train, y_train, sample_weight = weights)
+        # predict OOB
+        idx_oob = np.full(U, True)
+        idx_oob[idx_train] = False
+        _pred = clf.predict_proba(X_poblacion[idx_oob,:])[:,clf.classes_ == 1].ravel()
+        pred[idx_oob] += _pred
+        n[idx_oob] += 1
+        # update weight vector
+        if t > (T*l1):
+            _wupdate = np.zeros(U)
+            _wupdate[idx_oob] = _pred
+            w_poblacion += (-_wupdate/T*l2) 
+    scores = pred / n
+    return scores
